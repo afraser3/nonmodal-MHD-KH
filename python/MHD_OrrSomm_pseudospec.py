41cbc097a0876644107ccbc29137419c236a689e
Adrian (macbook) <adfraser@ucsc.edu>
2021-06-02 14:16:12 -0700

    Successfully calculates and plots pseudospectrum for given parameters

--- python/MHD_OrrSomm_pseudospec.py
+++ python/MHD_OrrSomm_pseudospec.py
@@ -4,23 +4,17 @@ I did very little testing of the viscous/resistive system, so it needs to be che
 (I have verified that it returns a reasonable eigenvalue for the current set of parameters.)
 It could definitely also use cleaning up.
 """
-# from eigentools import Eigenproblem
+from eigentools import Eigenproblem
 from dedalus import public as de
 import numpy as np
+from matplotlib import pyplot as plt
 
 Reynolds = 500.0
 mReynolds = 500.0
-Nz = 128
+Nz = 128  # when this was 64, EP discarded the physical unstable mode (relative error of 0.079%)
 Lz = 10.0*np.pi
-kx = 0.2
+kx = 0.4
 MA = 10.0
-
-solvedense = False
-nev = 1
-findstable = False
-energy_norm = True
-savemodes = False
-basefolder = 'savedmodes/'
 
 
 def energy_norm(X1, X2):
@@ -48,7 +42,7 @@ else:
 z_basis = de.Chebyshev('z', Nz, interval=(-0.5 * Lz, 0.5 * Lz))
 domain = de.Domain([z_basis], grid_dtype=np.complex128)
 probvars = ['phi', 'phi_z', 'psi', 'psi_z']
-if inviscid == False:
+if not inviscid:
     probvars.append('uz')
     probvars.append('uzz')
 else:
@@ -68,21 +62,23 @@ problem.parameters['DU'] = nccDU
 problem.parameters['MA2'] = MA ** 2.0  # Alfven Mach number squared
 problem.parameters['kx'] = kx
 problem.parameters['Lz'] = Lz
-if inviscid == False:
+if not inviscid:
     problem.parameters['Re'] = Reynolds
-if ideal == False:
+if not ideal:
     problem.parameters['Rm'] = mReynolds
 problem.substitutions['dx(A)'] = "1.0j*kx*A"
-problem.substitutions['dt(A)'] = "1.0j*omega*A"
+# problem.substitutions['dt(A)'] = "1.0j*omega*A"
+# problem.substitutions['dt(A)'] = "sigma*A"  # was having trouble with EP.calc_ps using omega
+problem.substitutions['dt(A)'] = "-1.0j*omega*A"
 problem.substitutions['u'] = 'phi_z'
 problem.substitutions['w'] = "-dx(phi)"
 problem.substitutions['Bz'] = "-dx(psi)"
 problem.substitutions['Bx'] = "psi_z"
 problem.substitutions['Jz'] = "dx(dx(psi)) + dz(Bx)"
-if inviscid == False:
+if not inviscid:
     problem.substitutions['zeta'] = "dx(dx(phi)) + uz"
     problem.substitutions['zetaz'] = "dx(dx(u)) + uzz"
-if inviscid == False:
+if not inviscid:
     problem.add_equation(
         "dt(zeta) - 1/Re*(dx(dx(zeta)) + dz(zetaz)) + U*dx(zeta) - dx(phi)*dz(DU) - 1/MA2*(dx(Jz)) = 0")
     problem.add_equation("uz - dz(u) = 0")
@@ -90,7 +86,7 @@ if inviscid == False:
 else:
     problem.add_equation("dt(zeta) + U*dx(zeta) - dx(phi)*dz(DU) - 1/MA2*(dx(Jz)) = 0")
     problem.add_equation("zeta - dx(dx(phi)) - dz(phi_z) = 0")
-if ideal == False:
+if not ideal:
     problem.add_equation("dt(psi) - 1/Rm*Jz + U*dx(psi) - dx(phi) = 0")
 else:
     problem.add_equation("dt(psi) + U*dx(psi) - dx(phi) = 0")
@@ -99,91 +95,35 @@ problem.add_equation("psi_z - dz(psi) = 0")
 
 problem.add_bc("left(w) = 0")
 problem.add_bc("right(w) = 0")
-if inviscid == False:
-    problem.add_bc("left(dz(w)) = 0")
+if not inviscid:
+    problem.add_bc("left(dz(w)) = 0")  # no-slip, co-moving (with base flow U_0) boundaries
     problem.add_bc("right(dz(w)) = 0")
-problem.add_bc("right(Bz) = 0")
+problem.add_bc("right(Bz) = 0")  # perfectly conducting boundaries
 problem.add_bc("left(Bz) = 0")
 
 print("done with BCs")
 
-solver = problem.build_solver()
-if solvedense == True:
-    solver.solve_dense(solver.pencils[0])
-else:  # very rough guess
-    if kx < 0.5:
-        guess = -1.0j * 0.5 * kx  # * np.sqrt(1.0 - (2.0/MA)**2)  # from palotti eq 7
-    else:
-        guess = -1.0j * 0.5 * (1.0 - kx)  # * np.sqrt(1.0-(2.0/MA)**2)
+EP = Eigenproblem(problem, grow_func=lambda x: x.imag, freq_func=lambda x: x.real)
+# EP.solve(sparse=False)
+# print("done with EP.solve()")
 
-    if findstable == True:
-        guess = -guess
-    solver.solve_sparse(solver.pencils[0], nev, 2.0 * guess, rebuild_coeffs=False)
+k = 100
 
-ev = solver.eigenvalues
-vecs = solver.eigenvectors
+psize = 100
+real_points = np.linspace(-0.5, 0.5, psize)
+imag_points = np.linspace(-0.5, 0.5, psize)
+EP.calc_ps(k, (real_points, imag_points), inner_product=energy_norm)
 
-# normalize phase of eigenvectors
-phase = lambda Z: Z / np.abs(Z)
-solver.eigenvectors /= phase(solver.eigenvectors[0:1, :])
-if energy_norm == True:
-    energies = compute_energies(solver)
-    solver.eigenvectors /= np.sqrt(energies)
-    vecs = solver.eigenvectors
-
-# end_time = time.time()
-# logger.info('Run time: %.2f sec' % (end_time - start_time))
-
-
-if savemodes == True:
-    sort = True
-    if sort == True:
-        # Remove nan and inf eigenvalues and sort them w/ largest gamma first
-        # -inf first, then smallest gamma, nans/+infs last
-        # order = np.argsort(-np.imag(ev))
-        # sort instead by increasing |omega|
-        order = np.argsort(np.abs(np.real(ev)))
-        while np.isfinite(ev[order[0]]) == False:  # take leading -infs
-            order = np.append(order[1:], order[0])  # and put them in the back
-        # Finally, reorganize eigenvalues and vectors according to this order
-        # ev = ev[order]
-        # vecs = vecs[order]
-        # Trim trailing nans and infs
-        ##vecs = vecs[np.isfinite(ev)]
-        ##ev = ev[np.isfinite(ev)]
-        # order = order[np.isfinite(ev[order])]
-
-    if solvedense == False and findstable == True:
-        analysisc = solver.evaluator.add_file_handler(basefolder + 'Eigenmodes_c_stable', iter=np.inf)
-        analysisc.add_system(solver.state, layout='c')
-
-        analysisg = solver.evaluator.add_file_handler(basefolder + 'Eigenmodes_g_stable', iter=np.inf)
-        analysisg.add_system(solver.state, layout='g')
-
-        for i in range(len(order)):
-            # if np.isfinite(ev[i]):
-            solver.set_state(order[i])
-            if i == 0:
-                analysisc.create_current_file()
-                analysisg.create_current_file()
-            solver.evaluator.evaluate_handlers([analysisc, analysisg], world_time=kx,
-                                               wall_time=np.imag(ev[order[i]]), sim_time=np.real(ev[order[i]]),
-                                               timestep=order[i], iteration=i)
-
-    else:
-        analysisc = solver.evaluator.add_file_handler(basefolder + 'Eigenmodes_c', iter=np.inf)
-        analysisc.add_system(solver.state, layout='c')
-
-        analysisg = solver.evaluator.add_file_handler(basefolder + 'Eigenmodes_g', iter=np.inf)
-        analysisg.add_system(solver.state, layout='g')
-
-        for i in range(len(order)):
-            # if np.isfinite(ev[i]):
-            solver.set_state(order[i])
-            if i == 0:
-                analysisc.create_current_file()
-                analysisg.create_current_file()
-            solver.evaluator.evaluate_handlers([analysisc, analysisg], world_time=kx,
-                                               wall_time=np.imag(ev[order[i]]), sim_time=np.real(ev[order[i]]),
-                                               timestep=order[i], iteration=i)
-
+plt.plot(np.real(EP.evalues_low), np.imag(EP.evalues_low), 'x', label=r'evalues_low')
+plt.plot(np.real(EP.evalues_high), np.imag(EP.evalues_high), '+', label=r'evalues_high')
+if len(EP.evalues) > 0:
+    plt.plot(np.real(EP.evalues), np.imag(EP.evalues), '.', label=r'evalues')
+plt.contour(EP.ps_real, EP.ps_imag, np.log10(EP.pseudospectrum), levels=np.arange(-8, 0))
+plt.colorbar()
+plt.legend()
+plt.ylim((-0.5, 0.5))
+plt.xlim((-0.5, 0.5))
+plt.axhline(0, color='k', alpha=0.2)
+plt.xlabel('real (oscillating) frequency')
+plt.ylabel('growth rate')
+plt.show()
